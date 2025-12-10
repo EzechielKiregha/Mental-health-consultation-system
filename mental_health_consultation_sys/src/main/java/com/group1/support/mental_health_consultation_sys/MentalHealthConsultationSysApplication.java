2278407b3fc7b83ebe24c422a10de8a364b78718
EzechielKiregha <ekiregha@gmail.com>
2025-12-10 20:31:56 +0200

    Spring initialization and pom dependencies configuration

---
+++ mental_health_consultation_sys/src/main/java/com/group1/support/mental_health_consultation_sys/MentalHealthConsultationSysApplication.java
@@ -0,0 +1,13 @@
+package com.group1.support.mental_health_consultation_sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentalHealthConsultationSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentalHealthConsultationSysApplication.class, args);
+	}
+
+}
---
+++ mental_health_consultation_sys/src/test/java/com/group1/support/mental_health_consultation_sys/MentalHealthConsultationSysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group1.support.mental_health_consultation_sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentalHealthConsultationSysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
